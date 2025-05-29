16a5be8ed122149008a39784359bc086d5eafef9
viral <viralmalaviya.inexture@gmail.com>
2025-05-29 14:21:59 +0530

    old integrstion

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import json
 import pandas as pd  # Added for CSV/XLSX parsing
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 users_db = {
